e684fa9c9924300690949646f52e87eae306581f
Alexey <mister.skorpion4ik@mail.ru>
2025-11-26 10:20:22 +0500

    finish app users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,19 @@
-# from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import User
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+    def setUp(self):
+        self.email = "test@user.ru"
+        self.password = "1234"
+
+    def test_user_registration(self):
+        data = {
+            "email": self.email,
+            "password": self.password,
+        }
+        response = self.client.post("/users/register", data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email="test@user.ru").exists())
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-# from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        password = serializer.validated_data.get("password")
+        user.set_password(password)
+        user.is_active = True
+        user.save()
